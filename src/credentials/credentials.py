70719baa1ceda4b09430f80c69e5b0e7d49b209b
Prashant Goel <56596612+Prashant1806@users.noreply.github.com>
2023-11-17 00:58:58 +0530

    Update credentials.py

--- src/credentials/credentials.py
+++ src/credentials/credentials.py
@@ -13,16 +13,28 @@ class CredentialsManager:
 
     def store_credentials(self):
         try:
-            # Prompt user for credentials
+            # Prompt user for AWS account ID
             aws_account_id = input("Enter your AWS account ID: ")
+
+            # Prompt user for AWS access key ID
             aws_access_key_id = input("Enter your AWS access key ID: ")
+
+            # Prompt user for AWS secret access key
             aws_secret_access_key = getpass("Enter your AWS secret access key: ")
+
+            # Prompt user for Azure subscription ID
+            azure_subscription_id = input("Enter your Azure subscription ID: ")
+
+            # Prompt user for Google Cloud project ID
+            google_cloud_project_id = input("Enter your Google Cloud project ID: ")
 
             # Create a dictionary of credentials
             credentials = {
                 'aws_account_id': aws_account_id,
                 'aws_access_key_id': aws_access_key_id,
-                'aws_secret_access_key': aws_secret_access_key
+                'aws_secret_access_key': aws_secret_access_key,
+                'azure_subscription_id': azure_subscription_id,
+                'google_cloud_project_id': google_cloud_project_id
             }
 
             # Encrypt and store credentials to the file
@@ -49,3 +61,14 @@ class CredentialsManager:
 
 if __name__ == '__main__':
     # Replace with your actual credentials_file_path
+    credentials_file_path = 'credentials.json'
+
+    # Create an instance of the CredentialsManager class
+    credentials_manager = CredentialsManager(credentials_file_path)
+
+    # Example usage: Store credentials
+    credentials_manager.store_credentials()
+
+    # Example usage: Retrieve credentials
+    retrieved_credentials = credentials_manager.retrieve_credentials()
+    print('Retrieved credentials:', retrieved_credentials)
